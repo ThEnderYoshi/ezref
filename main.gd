c88253f55006b9cd9ca5773a7761d4b622991756
ThEnderYoshi <ranesword@gmail.com>
2025-12-05 00:57:01 -0300

    Improved display settings
    - Display settings now have a dedicated popup.
    - Added the ability to change the background between checkered (light/dark) and transparent.

--- main.gd
+++ main.gd
@@ -7,16 +7,13 @@ var _should_show_ui := 0
 @onready var _the_image := %TheImage as Sprite2D
 @onready var _ui_root := %Ui as CanvasLayer
 @onready var _open_image := %OpenImage as FileDialog
-@onready var _filter_popup := (%Filter as OptionButton).get_popup()
 
 
 func _ready() -> void:
 	var window := get_window()
 	window.mouse_entered.connect(_incr_should_show_ui)
 	window.mouse_exited.connect(_decr_should_show_ui)
-	
 	_ui_root.hide()
-	_filter_popup.visibility_changed.connect(_on_filter_popup_visibility_changed)
 
 
 func _input(event: InputEvent) -> void:
@@ -44,13 +41,6 @@ func _decr_should_show_ui() -> void:
 		_ui_root.hide()
 
 
-func _on_filter_item_selected(index: int) -> void:
-	_the_image.texture_filter = (
-			CanvasItem.TEXTURE_FILTER_LINEAR if index == 0
-			else CanvasItem.TEXTURE_FILTER_NEAREST
-	)
-
-
 func _on_open_pressed() -> void:
 	_incr_should_show_ui()
 	_open_image.popup_centered()
@@ -73,11 +63,6 @@ func _on_open_image_file_selected(path: String) -> void:
 
 func _on_open_image_canceled() -> void:
 	_decr_should_show_ui()
-
-
-func _on_filter_popup_visibility_changed() -> void:
-	if not _filter_popup.visible:
-		_decr_should_show_ui()
 
 
 func _on_minimize_pressed() -> void:
